6cf08c165388647c2d4f5d142fced117f9a70f20
romashenkkko <70690141+romashenkkko@users.noreply.github.com>
2020-09-15 15:47:07 +0300

    Add files via upload

---
+++ 15.09.2020.py
@@ -0,0 +1,6 @@
+print("Numele, prenumele:Elena Romasenco")
+print("Data nasterii: 03.06.2004")
+print("Email: lenaromasenco@gmail.com")
+print("Adresa: Konev 42")
+print("Locul de studii: Liceul teoretic Mihai Eminescu")
+print("Numarul de telefon: 079143702")
